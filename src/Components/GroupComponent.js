de116b14f9ac96f8f8f1fbee835e8d0497d27fbb
Shivaraj Bakale <shivarajapple@Shivarajs-MacBook-Pro.local>
2019-11-26 13:13:39 +0530

    Loader added to groups

--- src/Components/GroupComponent.js
+++ src/Components/GroupComponent.js
@@ -9,6 +9,7 @@ import ModalButton from './ModalButton';
 import { loadMore } from '../store/Actions';
 import { throttle, debounce } from 'redux-saga/effects';
 import ScrollListener from 'react-bottom-scroll-listener';
+import { SpinLoader } from 'react-css-loaders';
 
 
 class GroupComponent extends React.Component {
@@ -26,29 +27,30 @@ class GroupComponent extends React.Component {
     }
     render() {
         return (
-            <ScrollListener onBottom={()=>this.loadMore()}>
+            <ScrollListener onBottom={() => this.loadMore()}>
                 <div>
-                <div className="centeredCss" style={{ marginTop: '20px' }}>
-                    <div className="col-md-4">
-                        <Search />
+                    <div className="centeredCss" style={{ marginTop: '20px' }}>
+                        <div className="col-md-4">
+                            <Search />
+                        </div>
                     </div>
+                    {this.props.groups.length > 0 ? <div className="container marginate">
+                        <h1><u>Results</u></h1>
+                        <div className="centeredCss">
+                            <ModalButton id="Hello" name="View Chart" />
+                            <Modal modalID="Hello" title="First Modal">
+                                <Chart data={this.props.groups} />
+                            </Modal>
+                        </div>
+                        <div className="w-layout-grid grid">
+                            {this.props.groups.map((item, index) => (
+                                <GroupCard data={item} key={index}></GroupCard>
+                            ))}
+                            {this.props.scrolling ? <SpinLoader /> : null}
+                        </div>
+                        {/*                     <button className="btn btn-success" onClick={this.loadMore}>Load More</button> */}
+                    </div> : null}
                 </div>
-                {this.props.groups.length > 0 ? <div className="container marginate">
-                    <h1><u>Results</u></h1>
-                    <div className="centeredCss">
-                        <ModalButton id="Hello" name="View Chart" />
-                        <Modal modalID="Hello" title="First Modal">
-                            <Chart data={this.props.groups} />
-                        </Modal>
-                    </div>
-                    <div className="w-layout-grid grid">
-                        {this.props.groups.map((item, index) => (
-                            <GroupCard data={item} key={index}></GroupCard>
-                        ))}
-                    </div>
-                    {/*                     <button className="btn btn-success" onClick={this.loadMore}>Load More</button> */}
-                </div> : null}
-            </div>
             </ScrollListener>
         );
     }
@@ -58,7 +60,8 @@ const mapStateToProps = (state) => {
         groups: state.groups,
         searchQuery: state.searchQuery,
         currentPage: state.currentPage,
-        scrolling: state.scrolling
+        scrolling: state.scrolling,
+        loading: state.loading
     }
 }
 
