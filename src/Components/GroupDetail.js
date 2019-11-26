2e999e69a23c03161a8542fe117bf708a857126d
Shivaraj Bakale <shivarajapple@Shivarajs-MacBook-Pro.local>
2019-11-26 13:24:09 +0530

    Documentation added

--- src/Components/GroupDetail.js
+++ src/Components/GroupDetail.js
@@ -2,18 +2,18 @@ import React from 'react';
 import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import './SearchBox.css';
-import { getImagesForGroup, getImagesForGroupCache , loadMoreImages} from '../store/Actions/index';
+import { getImagesForGroup, getImagesForGroupCache, loadMoreImages } from '../store/Actions/index';
 import StaggeredCard from './StaggeredCard';
 import DetailComponent from './DetailComponent';
 import Scroller from 'react-bottom-scroll-listener';
-import {SpinLoader} from 'react-css-loaders';
+import { SpinLoader } from 'react-css-loaders';
 
 
 class GroupDetail extends React.Component {
     constructor(props) {
         super(props);
-        this.state={
-            currentPage:1
+        this.state = {
+            currentPage: 1
         }
     }
     componentDidMount() {
@@ -25,14 +25,14 @@ class GroupDetail extends React.Component {
     navigateToOverview = (item) => {
         let data = this.props.history.location.pathname.split('/');
         let nsid = data[data.length - 1];
-        this.props.history.push(`/overview/${nsid}`, {name:"Hello World"});
+        this.props.history.push(`/overview/${nsid}`, { name: "Hello World" });
     }
-    reachedBottom=()=>{
+    reachedBottom = () => {
         let data = this.props.history.location.pathname.split('/');
         let nsid = data[data.length - 1];
         console.log("I have reached the bottom");
-        this.props.loadMoreImages(nsid, this.state.currentPage+1);
-        this.setState({currentPage:this.state.currentPage+1});
+        this.props.loadMoreImages(nsid, this.state.currentPage + 1);
+        this.setState({ currentPage: this.state.currentPage + 1 });
     }
     render() {
         return (
@@ -47,7 +47,7 @@ class GroupDetail extends React.Component {
                                 </div>
                             )) : null}
                             <div>
-                                {this.props.scrolling?<SpinLoader/>:null}
+                                {this.props.scrolling ? <SpinLoader /> : null}
                             </div>
                         </div>}
                     </div>
@@ -60,15 +60,15 @@ const mapStateToProps = (state) => {
     return {
         selectedGroupImages: state.selectedGroupImages,
         loading: state.loading,
-        selectedGroup: state.selectedGroup, 
-        scrolling:state.scrolling
+        selectedGroup: state.selectedGroup,
+        scrolling: state.scrolling
     }
 }
 
 const mapDispatchToProps = (dispatch) => {
     return {
-        getImagesForGroup: (data) => dispatch(getImagesForGroupCache(data)), 
-        loadMoreImages:(nsid, page)=>dispatch(loadMoreImages(nsid, page))
+        getImagesForGroup: (data) => dispatch(getImagesForGroupCache(data)),
+        loadMoreImages: (nsid, page) => dispatch(loadMoreImages(nsid, page))
     }
 }
 
